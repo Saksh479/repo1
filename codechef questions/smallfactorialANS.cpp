bf5fd3b65893ddcec83c9915416054c8f6dd1adf
Saksham <sakshamsengar479@gmail.com>
2021-11-30 16:41:00 +0530

    added small factorials

--- codechef questions/smallfactorialANS.cpp
+++ codechef questions/smallfactorialANS.cpp
@@ -2,30 +2,9 @@
 using namespace std;
 int main()
 {
-     int num,input[100],i;
+     int num,fac[200],i;
      cin>>num;
-     for ( i = 0; i < num; i++)
-     {  
     
-         cin>>input[i];
-       
-     }
-     for ( i = 0; i < num; i++)
-     {
-         int j=1;
-         if (input[i]==0)
-         {
-             cout<<"1"<<;
-         }
-         else
-         {
-           while (input[i]!=0)
-         {
-            j*=input[i]--;
-         }
-         cout<<j<<endl;
-        }
-     }
      
      
      return(0);
---
+++ multiplicationarray.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int a[200],m,num,i=0,facnum;
+    cin>>m;
+    facnum=m;
+    while (m>0)
+    {
+        num=m%10;
+        m/=10;
+        a[i]=num;
+        i++;
+    }
+        
+    int k=0,pro,temp=0;
+    while (facnum>0)
+    {   
+        facnum--;
+         do
+            {
+                pro=a[k]*facnum+temp;
+                a[k]=pro%10;
+                k++;
+                temp=pro/10;
+             } while (temp!=0);
+    }
+    for ( int j = k-1; j>=0 ; j--)
+    {
+        cout<<a[j];
+        
+    }
+    
+    
+    
+    
+    return(0);
+}
+// 75
+// 1
